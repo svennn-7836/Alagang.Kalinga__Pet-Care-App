2ca04e0085e81c5ee1a9e98390dcc2b27478ef23
Claudio Abuloc III <abulociiiclaudio@gmail.com>
2023-06-24 03:21:21 +0800

    Opening Screen Button Working

--- app/src/main/java/com/example/alagatkalinga/MainActivity.kt
+++ app/src/main/java/com/example/alagatkalinga/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.alagatkalinga
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +12,12 @@ class MainActivity : AppCompatActivity() {
         installSplashScreen()
         setContentView(R.layout.activity_main)
 
+        val continueButton = findViewById<Button>(R.id.continueButton)
+        continueButton.setOnClickListener {
+            // Handle button click event
+            val intent = Intent(this@MainActivity, MenuActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/alagatkalinga/MenuActivity.kt
@@ -0,0 +1,14 @@
+package com.example.alagatkalinga
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+        // Add your activity setup code here
+    }
+}
