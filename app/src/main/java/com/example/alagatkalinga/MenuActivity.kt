de794535c2a86e016497afa20ceea70d55071f63
Claudio Abuloc III <abulociiiclaudio@gmail.com>
2023-06-24 03:38:38 +0800

    Recipe button to category working

--- app/src/main/java/com/example/alagatkalinga/MenuActivity.kt
+++ app/src/main/java/com/example/alagatkalinga/MenuActivity.kt
@@ -1,6 +1,8 @@
 package com.example.alagatkalinga
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MenuActivity : AppCompatActivity() {
@@ -8,6 +10,12 @@ class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        val recipeImage: ImageView = findViewById(R.id.recipeImage)
+        recipeImage.setOnClickListener {
+            val intent = Intent(this, RecipeCategoryActivity::class.java)
+            startActivity(intent)
+        }
 
         // Add your activity setup code here
     }
---
+++ app/src/main/java/com/example/alagatkalinga/RecipeCategoryActivity.kt
@@ -0,0 +1,13 @@
+package com.example.alagatkalinga
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RecipeCategoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category_recipe)
+
+        // Add your activity setup code here
+    }
+}
