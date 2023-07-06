07d05807c30449f88caccad58e51be69a737a14a
Claudio Abuloc III <abulociiiclaudio@gmail.com>
2023-07-07 05:31:36 +0800

    Created DogRecipe3 Activity

---
+++ app/src/main/java/com/example/alagatkalinga/DogRecipe3Activity.kt
@@ -0,0 +1,12 @@
+package com.example.alagatkalinga
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DogRecipe3Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dog_recipe3)
+    }
+}
