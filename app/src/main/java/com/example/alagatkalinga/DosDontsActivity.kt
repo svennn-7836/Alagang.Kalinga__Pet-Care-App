c33fa9dbd1ffa92560d206b3d3edadf57191934a
Claudio Abuloc III <abulociiiclaudio@gmail.com>
2023-07-06 15:28:39 +0800

    Working Back Button on Do's and Don'ts Category

--- app/src/main/java/com/example/alagatkalinga/DosDontsActivity.kt
+++ app/src/main/java/com/example/alagatkalinga/DosDontsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.alagatkalinga
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class DosDontsActivity : AppCompatActivity() {
@@ -8,6 +9,10 @@ class DosDontsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category_dosdonts)
 
+        val backButton: ImageView = findViewById(R.id.BackMenu1)
+        backButton.setOnClickListener {
+            onBackPressed()
+        }
         // Add your activity setup code here
     }
 }
