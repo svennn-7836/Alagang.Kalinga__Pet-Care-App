7fb4aafaf083b012151e311b8d6c2718eeac2562
Claudio Abuloc III <abulociiiclaudio@gmail.com>
2023-07-07 05:51:19 +0800

    created and implemented back button for dog recipe 2

--- app/src/main/java/com/example/alagatkalinga/DogRecipe2Activity.kt
+++ app/src/main/java/com/example/alagatkalinga/DogRecipe2Activity.kt
@@ -1,6 +1,7 @@
 package com.example.alagatkalinga
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class DogRecipe2Activity : AppCompatActivity() {
@@ -8,5 +9,10 @@ class DogRecipe2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dog_recipe2)
+
+        val backButton: ImageView = findViewById(R.id.BackRed2)
+        backButton.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
